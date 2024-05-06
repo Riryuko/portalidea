c076128450a6966c97bc34607b0b12309ef919f0
Riryuko <skt.rafael.01@gmail.com>
2024-05-06 00:23:35 -0300

    commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+from datetime import datetime
+
+app = Flask(__name__)
+
+def calcular_horas_disponiveis(data_inicial_str):
+    data_inicial = datetime.strptime(data_inicial_str, '%d-%m-%y')
+    data_atual = datetime.now()
+    diferenca_dias = (data_atual - data_inicial).days + 1 
+    horas_disponiveis = diferenca_dias * 10
+    return horas_disponiveis
+
+@app.route('/')
+def home():
+    data_inicial_usuario = '01-01-23' 
+    total_horas = calcular_horas_disponiveis(data_inicial_usuario)
+    return f"Total de horas disponíveis do usuário desde {data_inicial_usuario}: {total_horas} horas"
+
+@app.route('/horas_disponiveis', methods=['POST'])
+def get_horas_disponiveis():
+    data_inicial = request.json.get('data_inicial')
+    if data_inicial:
+        horas = calcular_horas_disponiveis(data_inicial)
+        return jsonify({'horas_disponiveis': horas}), 200
+    else:
+        return jsonify({'error': 'Data inicial não fornecida'}), 400
---
+++ wsgi.py
@@ -0,0 +1,8 @@
+"""Executando a aplicação"""
+
+from .main import app
+
+app = app()
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5005)
