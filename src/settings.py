d1a59141cd409675086a49965081e5fc4ccc7514
otupa <marianotupa@gmail.com>
2024-07-30 00:14:58 -0300

    Configurações Iniciais do projeto

---
+++ src/settings.py
@@ -0,0 +1,39 @@
+# pylint: disable=too-few-public-methods
+"""Configurações para o projeto
+Para passar determinadas variaveis e constantes para o sistemas
+esteremos utilizando objetos com diferentes propriedades para
+cada ambiente. Para setar esse ambiente va para
+"""
+
+import getpass
+import os
+from os.path import join
+
+from dotenv import load_dotenv
+
+
+class Config:
+    """Configurações globais para todo o projeto"""
+
+    load_dotenv()
+
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    UPLOAD_FOLDER = os.environ.get("MIDIA_PATH")
+    MIDIA_PATH = join(os.getcwd(), "midia")
+    ALLOWED_EXTENSIONS = {
+        "txt",
+    }
+    DATABASE_CONNECTION = os.environ.get("DATABASE_CONNECTION")
+
+
+class TestingConfig(Config):
+    """Ambiente de testes"""
+
+    DEBUG = False
+    TESTING = True
+
+
+class ProductionConfig(Config):
+    """Ambiente de produção"""
+
+    DEBUG = False
--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,8 @@
 """Executando a aplicação"""
 
-from main import app
+from src import init_app
 
-app = app
+app = init_app()
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5005)
